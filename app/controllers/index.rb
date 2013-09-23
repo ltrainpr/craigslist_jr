d6125b3f865e89b2008ace34eae6fb61ad2ae1be
Lionel Ramos <lionelramos@Lionels-MacBook-Pro.local>
2013-09-22 18:26:46 -0700

    load index page successful-test passes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,8 @@
-require_relative '/Users/lionelramos/Documents/activerecord_jr/config/environment.rb'
+require 'sinatra'
 
+def app
+	Sinatra::Application
+end
 get '/' do
   # Look in app/views/index.erb
   erb :index
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,8 +1,9 @@
-require '/Users/lionelramos/Documents/activerecord_jr/app/controllers/index.rb'
+require_relative '/Users/lionelramos/Documents/activerecord_jr/app/controllers/index.rb'
 require 'rack/test'
 set :environment, :test
 
 describe "Index Page" do
+	include Rack::Test::Methods
 	it "should load the home page" do
 		get '/' 
 		last_response.status.should == 200
